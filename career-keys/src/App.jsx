573cf75894a1cb215e4630ee43b052c8e8acd44d
alexwalland <alexanderwalland@gmail.com>
2024-02-07 19:55:49 +0000

    added jsearch api variables to .env file

--- career-keys/src/App.jsx
+++ career-keys/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(import.meta.env.VITE_jKey);
   return (
     <>
       <div>
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR

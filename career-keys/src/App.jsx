9b21b05992dd2d9cf9be6aa4528d3a8a5989dd46
Tejpatta <tejurawal@gmail.com>
2024-02-08 20:03:27 +0000

    update dependencies

--- career-keys/src/App.jsx
+++ career-keys/src/App.jsx
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
 import React from 'react';
 import Nav from "./components/Nav";
 import Jumbotron from "./components/Jumbotron";
 import "./index.css";
-=======
-import Nav from "./components/Nav"
-import Jumbotron from "./components/Jumbotron"
-import SearchBar from "./components/SearchBar"
->>>>>>> f4b14f7daa377117d3107bb60daac82ce68ee8a8
 
 export default function App() {
   return (
     <>
-    <SearchBar/>
+    <Nav/>
+    <Jumbotron/>
     </>
   )
 }

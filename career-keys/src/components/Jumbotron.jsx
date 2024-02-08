1bddd2ef75a09c820ba1e4c806d386d90c755c08
Polly <147508946+porlowska@users.noreply.github.com>
2024-02-08 22:36:57 +0000

    added styling and images to the website

--- career-keys/src/components/Jumbotron.jsx
+++ career-keys/src/components/Jumbotron.jsx
@@ -1,12 +1,12 @@
 export default function Jumbotron() {
     return <>
 
-<section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
-    <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Unlock the keyword power!</h1>
-        <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48"> Get your CV ready woth keywords </p>
-        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-            <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+<section className="bg-cover bg-no-repeat bg-[url('././images/jumbotron.jpg')] bg-gray-500 bg-blend-multiply">
+    <div className="px-10 mx-auto max-w-screen-md text-center py-24 md:py-56">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Make Every Word Count!</h1>
+        <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48"> Elevate Your Resume with Precision-Selected Keywords! </p>
+        <div className="flex flex-col space-y-4 sm:flex-row px-10 sm:justify-center sm:space-y-0">
+            <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-rose-950 rounded-lg bg-rose-400 hover:bg-rose-500 focus:ring-4 focus:ring-rose-300">
                 Search now!
                 <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
--- career-keys/src/components/Nav.jsx
+++ career-keys/src/components/Nav.jsx
@@ -2,17 +2,16 @@ import React from "react";
 
 export default function Nav() {
     return <>
-    <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+    <nav className="border-rose-50 bg-rose-50">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Career Keys</span>
+            <img src="././images/logo.png" className="h-12" alt="Career Keys logo" />
         </a>
         
-        <div className="w-full sm:block sm:w-auto" id="navbar-solid-bg">
-          <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 sm:space-x-8 rtl:space-x-reverse  sm:mt-0 sm:border-0 sm:bg-transparent dark:bg-gray-800 sm:dark:bg-transparent ">
+        <div className=" block w-auto " id="navbar-solid-bg">
+          <ul className="flex flex-col font-medium m-3 rounded-lg  space-x-8 rtl:space-x-reverse mr-10 border-2 border-rose-400 bg-transparent  p-1">
             <li>
-              <a href="#" className="block py-2 px-3 sm:p-0 text-white rounded " aria-current="page">Saved</a>
+              <a href="#" className=" py-2 px-3 p-0 text-rose-950 rounded " aria-current="page">Saved</a>
             </li>
           </ul>
         </div>

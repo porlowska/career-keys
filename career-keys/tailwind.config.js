d9f8d64b83ae0b505ddcf9b9f004196fc4c39bf6
Tejpatta <tejurawal@gmail.com>
2024-02-08 18:48:49 +0000

    add Flowbite dependecies

--- career-keys/tailwind.config.js
+++ career-keys/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 

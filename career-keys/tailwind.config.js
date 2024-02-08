75a052306877730972cbd391d0db0e42fe8d8980
alexwalland <alexanderwalland@gmail.com>
2024-02-08 19:49:42 +0000

    search bar on screen

--- career-keys/tailwind.config.js
+++ career-keys/tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./node_modules/flowbite/**/*.js"
+    "./src/**/*.{js,jsx,ts,tsx}'"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
   ],
 }
 

72b5d318e117655a89075b234c7e973bf3ac7b60
Tejpatta <tejurawal@gmail.com>
2024-02-09 19:47:45 +0000

    added tabbed cards for details - description and keywords - off job

--- career-keys/src/App.jsx
+++ career-keys/src/App.jsx
@@ -4,6 +4,7 @@ import SearchBar from "./components/SearchBar";
 import Jumbotron from "./components/Jumbotron";
 import Footer from './components/Footer';
 import JListing from './components/JobListing';
+import JobDetails from './components/JobDetails';
 import "./index.css";
 
 export default function App() {
@@ -12,7 +13,8 @@ export default function App() {
     <Nav/>
     <Jumbotron/>
     <SearchBar/>
-
+    <JListing/>
+    <JobDetails/>
     <Footer/>
     </>
   )

7dcaec1582d72a07a3d63eb1c5b7e8e1759e02bc
feliperyder <147994564+feliperyder@users.noreply.github.com>
2024-02-10 04:02:04 +0800

    Updated components

--- career-keys/src/components/main page/Homepage.jsx
+++ career-keys/src/components/main page/Homepage.jsx
@@ -4,11 +4,33 @@ import SearchBar from "./SearchBar";
 import Jumbotron from "./Jumbotron";
 import JListing from './JobListing';
 
+const fetch = require('node-fetch');
+
 export default function Homepage() {
+  const handleSearch=async (query)=>{
+
+    const url = `https://jsearch.p.rapidapi.com/search?query=${}&page=10&num_pages=10`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': import.meta.env.VITE_jKey,
+        'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+      }
+    };
+    
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <>
     <Jumbotron/>
-    <SearchBar/>
+    <SearchBar onSearch={handleSearch}/>
     <JListing/>
     </>
   )

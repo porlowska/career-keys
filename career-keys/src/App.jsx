9896ce0322d095d7974aac792e117dd5ec6338cc
alexwalland <alexanderwalland@gmail.com>
2024-02-09 18:30:57 +0000

    added job listing to page

--- career-keys/src/App.jsx
+++ career-keys/src/App.jsx
@@ -3,7 +3,7 @@ import Nav from "./components/Nav";
 import SearchBar from "./components/SearchBar";
 import Jumbotron from "./components/Jumbotron";
 import Footer from './components/Footer';
-
+import JListing from './components/JobListing';
 import "./index.css";
 
 export default function App() {
@@ -11,6 +11,7 @@ export default function App() {
     <>
     <Nav/>
     <Jumbotron/>
+    <SearchBar/>
 
     <Footer/>
     </>

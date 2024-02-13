8021f41a9807e00031d4a39f747d64cb14c471c4
Tejpatta <tejurawal@gmail.com>
2024-02-13 18:30:19 +0000

    attempt to construct openai api call

--- career-keys/src/components/mainpage/JobListing.jsx
+++ career-keys/src/components/mainpage/JobListing.jsx
@@ -3,43 +3,6 @@ import JobDetails from './JobDetails';
 
 export default function JobListing({jobSearch}) {
 
-    const [jDesc, setjDesc]=useState([])
-    const [keywords, setKeywords] = useState([]);
-    const getKeywords = async(text) => {
-        const url = "https://api.openai.com/v1/chat/completions";
-        const options = {
-            method: 'POST',
-            headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${import.meta.env.VITE_LKey}`, 
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",           
-            prompt:
-              'Extract keywords from this text. Make the first letter of every word uppercase and separate with commas:\n\n' +
-              text +
-              '',
-            temperature: 0.5,
-            max_tokens: 60,
-            top_p: 1.0,
-            frequency_penalty: 0.8,
-            presence_penalty: 0.0,
-          }),
-        };
-        try {
-          const response = await fetch(
-            url,
-            options
-          );
-          const json = await response.json();
-          console.log(json.choices[0].text.trim());
-          setKeywords(json.choices[0].text.trim());
-          setLoading(false);
-        } catch (error) {
-          console.error(error);
-        }
-    }
-
 return ( 
     <>
     <div className='container mb-2 w-2/6 pl-10'>

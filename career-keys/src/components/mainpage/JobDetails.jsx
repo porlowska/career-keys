38c666ea034c2fd034aa85ce2f0e748a06d37aa1
alexwalland <alexanderwalland@gmail.com>
2024-02-13 18:40:36 +0000

    removed variables to prevent job description from crashing site

--- career-keys/src/components/mainpage/JobDetails.jsx
+++ career-keys/src/components/mainpage/JobDetails.jsx
@@ -29,13 +29,13 @@ export default function JobDetails (job) {
                             <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                             </svg>
-                            <span className="leading-tight">{job.job.job_highlights.Qualifications[0]}</span>
+                            <span className="leading-tight"></span>
                         </li>
                         <li className="flex space-x-2 rtl:space-x-reverse items-center">
                             <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                             </svg>
-                            <span className="leading-tight">{job.job.job_highlights.Responsibilities[0]}</span>
+                            <span className="leading-tight"></span>
                         </li>
                     </ul>
                 </div>

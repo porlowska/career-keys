c1fbb1da3ff2f3e75100c6d6f3acf985d91540f3
Polly <147508946+porlowska@users.noreply.github.com>
2024-02-14 23:42:28 +0000

    image file path changed for logo.png

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ export default function Nav() {
     <nav className="border-rose-50 bg-white-50">
       <div className="max-w-screen-xl flex flex-row items-center justify-between mx-auto p-4">
         <a onClick={()=>window.location.reload()} className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="././images/logo.png" className="h-12" alt="Career Keys logo" />
+            <img src="../../images/logo.png" className="h-12" alt="Career Keys logo" />
         </a>
         
         </div>

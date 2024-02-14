69a39ab5d7e891978b706c05575d90d91b64198a
Tejpatta <tejurawal@gmail.com>
2024-02-14 18:59:26 +0000

    add background styling to job detail section

--- career-keys/src/App.jsx
+++ career-keys/src/App.jsx
@@ -3,13 +3,12 @@ import Nav from "./components/Nav";
 import Homepage from './components/mainpage/Homepage';
 import Footer from './components/Footer';
 import "./index.css";
+import JobDetails from './components/mainpage/JobDetails';
 
 export default function App() {
   return (
     <>
-    <Nav/>
-    <Homepage/>
-    <Footer/>
+    <JobDetails/>
     </>
   )
 }
--- career-keys/src/components/mainpage/JobDetails.jsx
+++ career-keys/src/components/mainpage/JobDetails.jsx
@@ -2,41 +2,26 @@
 export default function JobDetails ({job, words}) {
     return (
         <div id="description" className="col-span-1 pr-10">
-            <h1>{words}</h1>
-        <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 rounded-t-lg bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800" id="defaultTab" data-tabs-toggle="#defaultTabContent" role="tablist">
+        <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-rose-950 dark:border-gray-700">
+            <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 rounded-t-lg bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:bg-rose-950" id="defaultTab" data-tabs-toggle="#defaultTabContent" role="tablist">
                 <li className="me-2">
-                    <button id="about-tab" data-tabs-target="#about" type="button" role="tab" aria-controls="about" aria-selected="true" className="inline-block p-4 text-blue-600 rounded-ss-lg hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-blue-500">Description</button>
+                    <button id="about-tab" data-tabs-target="#about" type="button" role="tab" aria-controls="about" aria-selected="true" className="inline-block p-4 text-blue-600 rounded-ss-lg hover:bg-gray-100 dark:bg-rose-950 dark:hover:bg-gray-700 dark:text-blue-500">Description</button>
                 </li>
                 <li className="me-2">
                     <button id="services-tab" data-tabs-target="#services" type="button" role="tab" aria-controls="services" aria-selected="false" className="inline-block p-4 hover:text-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-gray-300">Keywords</button>
                 </li>
             </ul>
             <div id="defaultTabContent">
-                <div className="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="about" role="tabpanel" aria-labelledby="desc-tab">
-                    <h2 className="mb-3 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">{job.job_title}</h2>
-                    <h3 className="mb-3 text-xl font-extrabold tracking-tight text-gray-900 dark:text-white">{job.employer_name}</h3>
+                <div className="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-rose-950" id="about" role="tabpanel" aria-labelledby="desc-tab">
+                    <h2 className="mb-3 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">Test title</h2>
+                    <h3 className="mb-3 text-xl font-extrabold tracking-tight text-gray-900 dark:text-white">test name</h3>
                     <p className="mb-3 text-gray-900 dark:text-white">
-                        {job.job_description}
+                        description
                     </p>
                 </div>
-                <div className="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="services" role="tabpanel" aria-labelledby="key-tab">
+                <div className="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-rose-950" id="services" role="tabpanel" aria-labelledby="key-tab">
                     <h2 className="mb-5 text-2xl font-extrabold tracking-tight text-gray-900 dark:text-white">Keywords</h2>
-                    <p>{words}</p>
-                    <ul role="list" className="space-y-4 text-gray-500 dark:text-gray-400">
-                        <li className="flex space-x-2 rtl:space-x-reverse items-center">
-                            <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-                            </svg>
-                            <span className="leading-tight"></span>
-                        </li>
-                        <li className="flex space-x-2 rtl:space-x-reverse items-center">
-                            <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-                            </svg>
-                            <span className="leading-tight"></span>
-                        </li>
-                    </ul>
+                    <p className="text-white">words wordz</p>
                 </div>
             </div>
     </div> 
